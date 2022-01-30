b5b20e8960ac67a49a79a69e390bbc18b531cdda
veenayakAggarwal <veenayak.aggarwal@zemosolabs.com>
2022-01-30 20:44:13 +0530

    added test

--- graphql/userResolver.test.ts
+++ graphql/userResolver.test.ts
@@ -1,8 +1,19 @@
 import { expect } from 'chai';
+import { getUsers } from './userResolver';
+import { stub } from 'sinon';
+import * as userModel from '../models/UserModel';
+const User = userModel.userModel;
 
-it('test', () => {
-    const num1 = 3;
-    const num2 = 2;
+it('getUsers', async () => {
+    
+    let mock:any = stub(User, 'find');
+    mock.returns({
+        email: 'email',
+        password: 'pwd'
+    });
 
-    expect(num1 + num2).to.equal(5);
+    const result:any = await getUsers();
+
+    expect(result.email).to.equal('email');    
+    expect(result.password).to.equal('pwd');    
 });
